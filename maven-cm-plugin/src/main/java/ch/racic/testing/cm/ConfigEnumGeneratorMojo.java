2702f37fa16ccb016b90552bc660b22fec84bac3
rac2030 <m@rac.su>
2015-04-29 22:33:40 +0200

    First base

---
+++ maven-cm-plugin/src/main/java/ch/racic/testing/cm/ConfigEnumGeneratorMojo.java
@@ -0,0 +1,79 @@
+package ch.racic.testing.cm;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
+
+import java.io.File;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal touch
+ * @phase process-sources
+ */
+@Mojo(
+        name = "generateConfigEnum",
+        requiresProject = true,
+        defaultPhase = LifecyclePhase.NONE
+)
+@Execute(phase = LifecyclePhase.GENERATE_SOURCES)
+public class ConfigEnumGeneratorMojo extends AbstractMojo {
+
+    @Component
+    protected MavenProject project;
+
+    @Component
+    private Settings settings;
+
+    @Parameter(
+            required = true,
+            readonly = true,
+            defaultValue = "${project.build.directory}/generated-sources/configEnums",
+            property = "outputDir"
+    )
+    private File outputDir;
+
+    @Parameter(
+            required = true,
+            readonly = true,
+            defaultValue = "${project.groupId}",
+            property = "basePackage"
+    )
+    private String basePackage;
+
+    @Parameter(
+            required = true,
+            readonly = true,
+            defaultValue = "false",
+            property = "testSourceOnly"
+    )
+    private boolean testSourceOnly;
+
+    public void execute() throws MojoExecutionException {
+        // TODO generate something
+
+        // After all is generated, let's add it to the corresponding source root
+        if (testSourceOnly) loadGeneratedTestSources();
+        else loadGeneratedSources();
+
+    }
+
+    private void loadGeneratedTestSources() {
+        if (!settings.isInteractiveMode()) {
+            getLog().info(String.format("Adding generated configuration enums from %s to test compile source root",
+                    outputDir.getAbsolutePath()));
+        }
+        project.addTestCompileSourceRoot(outputDir.getAbsolutePath());
+    }
+
+    private void loadGeneratedSources() {
+        if (!settings.isInteractiveMode()) {
+            getLog().info(String.format("Adding generated configuration enums from %s to compile source root",
+                    outputDir.getAbsolutePath()));
+        }
+        project.addTestCompileSourceRoot(outputDir.getAbsolutePath());
+    }
+}
