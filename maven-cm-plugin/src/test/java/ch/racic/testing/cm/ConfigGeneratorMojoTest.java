ae878747c37dbd81edc315a7bf62df79fc3bb27d
rac2030 <m@rac.su>
2015-05-10 09:26:52 +0200

    prepared unit test

--- maven-cm-plugin/src/test/java/ch/racic/testing/cm/ConfigGeneratorMojoTest.java
+++ maven-cm-plugin/src/test/java/ch/racic/testing/cm/ConfigGeneratorMojoTest.java
@@ -1,5 +1,34 @@
 package ch.racic.testing.cm;
 
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+
 public class ConfigGeneratorMojoTest {
 
+    @Rule
+    public MojoRule rule = new MojoRule() {
+        @Override
+        protected void before() throws Throwable {
+        }
+
+        @Override
+        protected void after() {
+        }
+    };
+
+    @Test
+    public void testSomething()
+            throws Exception {
+        File pom = rule.getTestFile("src/test/resources/unit/project-to-test/pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        MyMojo myMojo = (MyMojo) rule.lookupMojo("touch", pom);
+        assertNotNull(myMojo);
+        myMojo.execute();
+
+        ...
+    }
 }
